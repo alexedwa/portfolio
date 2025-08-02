8da4f3cb5ef91199a90815065b4eae8a6164f7dd
alexedwa <alexedwa123@gmail.com>
2025-08-03 00:19:47 +0100

    Added Footer section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Intro from './components/Intro';
 import About from './components/About';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Intro />
         <About />
         <Projects />
+        <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return(
+        <div className="w-screen bg-gray-100">
+            <div className="p-7 text-center">
+                <a className="m-3 text-gray-600 hover:text-gray-400" 
+                href="https://github.com/alexedwa" target="_blank">GitHub</a>
+                
+                <a className="m-3 text-gray-600 hover:text-gray-400" 
+                href="https://www.linkedin.com/in/alexander-edwards-7072252b8/" target="_blank">LinkedIn</a>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
