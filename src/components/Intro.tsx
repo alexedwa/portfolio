20dd46659c065ad42a685cad5a04b3fe4da44650
alexedwa <alexedwa123@gmail.com>
2025-08-02 22:58:52 +0100

    Edited intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,7 +3,7 @@ function Intro() {
 
     return(
         <div className="h-svh flex items-center justify-center bg-gray-100">
-            <div className="rounded-sm p-10 bg-gray-200 m-2">
+            <div className="rounded-sm p-10 bg-gray-200 m-2 shadow-md hover:shadow-xs transition-shadow duration-300">
                 <p className="text-2g">Hello, I am Alex and welcome to my website!</p>
             </div>
         </div>
