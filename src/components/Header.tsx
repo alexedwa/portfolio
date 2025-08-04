db20a87250d0fe0a025215797297aa8443971510
alexedwa <alexedwa123@gmail.com>
2025-08-05 00:55:25 +0100

    Added enter and progress bar animation to header and intro

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,41 +5,58 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger
   } from "@/components/ui/dropdown-menu" 
+import { motion, useScroll } from 'motion/react';
 
 function Header() {
+    const { scrollYProgress } = useScroll();
 
     return(
-        <header className="fixed w-screen top-0 z-1 border-2 border-b-gray-300 bg-gray-200">
-            <div className="container mx-auto px-4">
-                <div className="flex items-center justify-between h-16">
-                    <div className="flex items-center space-x-2">
-                        <span className="text-xl font-bold text-foreground">Alexander Edwards</span>
+        <header>
+            <div className="fixed w-screen top-0 z-1 border-2 border-b-gray-300 bg-gray-200">
+                <div className="container mx-auto px-4">
+                    <div className="flex items-center justify-between h-16">
+                        <div className="flex items-center space-x-2">
+                            <span className="text-xl font-bold text-foreground">Alexander Edwards</span>
+                        </div>
+
+                        {/* Desktop menu buttons */}
+                        <nav className="hidden md:flex item-center space-x-3">
+                            <a className="hover:bg-gray-300 transition-colors p-1 rounded-sm" href="#about">About Me</a>
+                            <Separator orientation="vertical"/>
+                            <a className="hover:bg-gray-300 transition-colors p-1 rounded-sm" href="#projects">Projects</a>
+                            <Separator orientation="vertical" />
+                            <a className="hover:bg-gray-300 transition-colors p-1 rounded-sm" href="mailto:alexedwa123@gmail.com">Contact</a>
+                            <Separator orientation="vertical" />
+                            <a className="hover:bg-gray-300 transition-colors p-1 rounded-sm" href="https://github.com/alexedwa" target="_blank">GitHub</a>
+                        </nav>
+
+                        {/* Mobile dropdown menu */}
+                        <nav className="block md:hidden">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>☰</DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuItem><a href="#about">About Me</a></DropdownMenuItem>
+                                    <DropdownMenuItem><a href="#projects">Projects</a></DropdownMenuItem>
+                                    <DropdownMenuItem><a href="mailto:alexedwa123@gmail.com">Contacts</a></DropdownMenuItem>
+                                    <DropdownMenuItem><a href="https://github.com/alexedwa" target="_blank">GitHub</a></DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </nav>
                     </div>
-
-                    {/* Desktop menu buttons */}
-                    <nav className="hidden md:flex item-center space-x-3">
-                        <a className="hover:bg-gray-300 transition-colors p-1 rounded-sm" href="#about">About Me</a>
-                        <Separator orientation="vertical"/>
-                        <a className="hover:bg-gray-300 transition-colors p-1 rounded-sm" href="#projects">Projects</a>
-                        <Separator orientation="vertical" />
-                        <a className="hover:bg-gray-300 transition-colors p-1 rounded-sm" href="mailto:alexedwa123@gmail.com">Contact</a>
-                        <Separator orientation="vertical" />
-                        <a className="hover:bg-gray-300 transition-colors p-1 rounded-sm" href="https://github.com/alexedwa" target="_blank">GitHub</a>
-                    </nav>
-
-                    {/* Mobile dropdown menu */}
-                    <nav className="block md:hidden">
-                        <DropdownMenu>
-                            <DropdownMenuTrigger>☰</DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                                <DropdownMenuItem><a href="#about">About Me</a></DropdownMenuItem>
-                                <DropdownMenuItem><a href="#projects">Projects</a></DropdownMenuItem>
-                                <DropdownMenuItem><a href="mailto:alexedwa123@gmail.com">Contacts</a></DropdownMenuItem>
-                                <DropdownMenuItem><a href="https://github.com/alexedwa" target="_blank">GitHub</a></DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </nav>
                 </div>
+                {/* Progress bar */}
+                    <motion.div className="fixed w-screen"
+                        style={{
+                            scaleX: scrollYProgress,
+                            position: "fixed",
+                            top: 68,
+                            left: 0,
+                            right: 0,
+                            height: 10,
+                            originX: 0,
+                            backgroundColor: "#71717A"
+                        }}>     
+                </motion.div>
             </div>
         </header>
     )
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,12 +1,13 @@
+import { motion } from 'motion/react';
 
 function Intro() {
 
     return(
-        <div className="h-svh w-screen flex items-center justify-center bg-transparent">
+        <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} className="h-svh w-screen flex items-center justify-center bg-transparent">
             <div className="rounded-sm p-10 bg-gray-200 m-2 shadow-md hover:shadow-xs transition-shadow duration-300">
                 <p className="text-2g">Hello, I am Alex and welcome to my website!</p>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
