5e510a5c28ccbb0c3a5c24e4304345ef2eb3953f
alexedwa <alexedwa123@gmail.com>
2025-08-05 04:36:05 +0100

    Fixed grammar

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,8 +4,8 @@ function Intro() {
 
     return(
         <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} className="h-svh w-screen flex items-center justify-center bg-transparent">
-            <div className="rounded-sm p-10 bg-gray-200 m-2 shadow-md hover:shadow-xs transition-shadow duration-300">
-                <p className="text-2g">Hello, I am Alex and welcome to my website!</p>
+            <div className="rounded-sm p-10 bg-white m-2 shadow-xl">
+                <p className="text-2g">Hello, I am Alex, and welcome to my website!</p>
             </div>
         </motion.div>
     )
