cf7d09af78009a868637fa42c1a3cfb5299257e0
alexedwa <alexedwa123@gmail.com>
2025-08-06 01:18:54 +0100

    Fixed border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return(
-        <div className="w-screen border-t-2 bg-gray-200">
+        <div className="w-screen border-t-2 border-t-gray-300 bg-gray-200">
             <div className="p-7 text-center">
                 <a className="m-3 text-gray-600 hover:text-gray-400" 
                 href="https://github.com/alexedwa" target="_blank">GitHub</a>
